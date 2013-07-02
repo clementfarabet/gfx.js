7e932eef48b879bf97ac3f56f7bd89e858dd2023
Clement Farabet <clement.farabet@gmail.com>
2013-07-01 23:27:40 -0400

    Always open browser.

--- clients/torch/js.lua
+++ clients/torch/js.lua
@@ -348,13 +348,14 @@ local status = io.popen('curl -s http://localhost:8000'):read('*all'):gsub('%s*'
 if status == '' then
    -- start up server:
    os.execute('node "' .. os.getenv('HOME') .. '/.tty.js/server.js" > "' .. os.getenv('HOME') .. '/.tty.js/server.log" &')
-   -- open up browser:
-   if jit.os == 'OSX' then
-      os.execute('open http://localhost:8000/')
-   end
    print('[tty.js] server started on port 8000, graphics will be rendered into http://localhost:8000')
 else
    print('[tty.js] server already running on port 8000, graphics will be rendered into http://localhost:8000')
+end
+
+-- open up browser:
+if jit.os == 'OSX' then
+   os.execute('open http://localhost:8000/')
 end
 
 return js
