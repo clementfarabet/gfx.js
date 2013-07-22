3f640135a24e1dad9ff470184034ef955985de9d
Clement Farabet <clement.farabet@gmail.com>
2013-07-21 21:58:14 -0400

    Test for distribution chart.

---
+++ clients/torch/test-distributions.lua
@@ -0,0 +1,19 @@
+require 'sys'
+require 'gfx.go'
+
+sys.sleep(1)
+
+N = 500
+
+n = torch.Tensor(N,2):normal(0,1)
+u = torch.Tensor(N,2):uniform(-1,1)
+
+gfx.chart({
+   {values=n, key='Normal'},
+   {values=u, key='Uniform'},
+}, {
+   width = 1024,
+   height = 768,
+   chart = 'scatter',
+})
+
